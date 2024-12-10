ca65da9c7c88b28855d4f957c7e2b770b4c7715f
Sanjit Basker <tijnasreksab@gmail.com>
2024-12-10 17:59:22 -0600

    WA oof

--- day_6.py
+++ day_6.py
@@ -2,44 +2,105 @@ import sys
 
 
 def parse(lines):
-    matches = [[(c == ".", c == "^") for c in line.strip()] for line in lines]
-    return [[m[0] for m in row] for row in matches], (
-        max(range(0, len(matches)), key=lambda i: int(any(m[1] for m in matches[i]))),
-        max(
-            range(0, len(matches[0])), key=lambda i: int(any(m[i][1] for m in matches))
-        ),
-    )
+  matches = [[(c == ".", c == "^") for c in line.strip()] for line in lines]
+  return [[m[0] for m in row] for row in matches], (
+    max(
+      range(0, len(matches)), key=lambda i: int(any(m[1] for m in matches[i]))
+    ),
+    max(
+      range(0, len(matches[0])),
+      key=lambda i: int(any(m[i][1] for m in matches)),
+    ),
+  )
 
 
 def part1(traversable, start):
-    traversable[start[0]][start[1]] = True
-    direction = (-1, 0)
+  traversable[start[0]][start[1]] = True
+  direction = (-1, 0)
 
-    def turn(direction):
-        return (direction[1], -direction[0])
+  def turn(direction):
+    return (direction[1], -direction[0])
 
-    def in_bounds(i, j):
-        return 0 <= i < len(traversable) and 0 <= j < len(traversable[0])
+  def in_bounds(i, j):
+    return 0 <= i < len(traversable) and 0 <= j < len(traversable[0])
 
+  pos = start
+  seen = set((pos,))  # pos
+  while True:
+    match tuple(u + v for u, v in zip(pos, direction)):
+      case (x, y) as next_pos:
+        if (ib := in_bounds(x, y)) and traversable[x][y]:
+          pos = next_pos
+          seen.add(pos)
+        elif ib:
+          direction = turn(direction)
+        else:
+          return len(seen)
+      case _:
+        print("bad!")
+
+
+def part2(traversable, start):
+  traversable[start[0]][start[1]] = True
+
+  def turn(direction):
+    return (direction[1], -direction[0])
+
+  def in_bounds(i, j):
+    return 0 <= i < len(traversable) and 0 <= j < len(traversable[0])
+
+  def continue_simulation(start, direction, steps) -> bool:
     pos = start
-    seen = set((pos,))  # pos
-    while True:
+    seen = set(steps)
+    loop = None
+    while loop is None:
+      for _ in range(4):
         match tuple(u + v for u, v in zip(pos, direction)):
-            case (x, y) as next_pos:
-                if (ib := in_bounds(x, y)) and traversable[x][y]:
-                    pos = next_pos
-                    seen.add(pos)
-                elif ib:
-                    direction = turn(direction)
-                else:
-                    return len(seen)
-            case _:
-                print("bad!")
+          case (x, y) as tentative_next_pos:
+            if not (ib := in_bounds(x, y)):
+              break
+            elif not traversable[x][y]:
+              direction = turn(direction)
+              continue
+            else:
+              break
+      else:
+        print("infinite loop on a single spot, seems sus")
+      if (pos, direction) in seen:
+        loop = True
+      elif ib:
+        seen.add((pos, direction))
+        steps.append((pos, direction))
+        pos = tentative_next_pos
+      else:
+        seen.add((pos, direction))
+        steps.append((pos, direction))
+        loop = False
+    return loop
 
+  steps = []
+  assert continue_simulation(start, (-1, 0), steps) is False
 
-def part2():
-    return ()
+  ans = set()
+  seen = set((start,))
+  # for each subsequence consisting of the first i steps, determine if an
+  # obstacle can be used to repeat one of those steps
+  for i in range(len(steps) - 1):
+    next_pos = steps[i + 1][0]  # propose putting an obstacle here
+    if next_pos in seen:
+      continue
+    else:
+      seen.add(next_pos)
+    traversable[next_pos[0]][next_pos[1]] = False
+    temp = steps[:i]
+    if continue_simulation(*steps[i], temp):
+      ans.add(next_pos)
+    traversable[next_pos[0]][next_pos[1]] = True
+  # for i, j in ans:
+  #   traversable[i][j] = True
+  #   assert continue_simulation(start, (-1, 0), [])
+  return len(ans)
 
 
 if __name__ == "__main__":
-    print(part1(*parse(sys.stdin)))
+  print(part2(*parse(sys.stdin)))
