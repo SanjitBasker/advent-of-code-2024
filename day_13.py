442dbed95aabaa012c6a2c7578548c36032db035
Sanjit Basker <tijnasreksab@gmail.com>
2024-12-15 17:31:02 -0600

    13p2

--- day_13.py
+++ day_13.py
@@ -2,6 +2,7 @@ import re
 import sys
 from itertools import batched
 import numpy as np
+from math import gcd
 
 
 def parse(lines):
@@ -46,9 +47,29 @@ def part1(groups):
   return ans
 
 
-def part2():
-  return ()
+def part2(groups):
+  ans = 0
+  for u, v, target in groups:
+    if u[0] * v[1] == u[1] * v[0]:
+      vx, vy = target
+      vx += 10000000000000
+      vy += 10000000000000
+      for i in range(u[0] * v[0]):
+        if vx % v[0] == 0 and vy % v[1] == 0 and 0 <= vx // v[0] == vy // v[1]:
+          ans += i * 3 + vx // v[0]
+          break
+        vx -= u[0]
+        vy -= u[1]
+    else:
+      m = np.array([u, v]).T
+      t = np.array(target).T
+      t += 10000000000000
+      sol = np.linalg.solve(m, t).T
+      cast = np.round(sol).astype(np.int64)
+      if np.allclose(m @ cast, t, 0, 0.5) and all(0 <= cast):
+        ans += cast @ np.array([3, 1]).T
+  return ans
 
 
 if __name__ == "__main__":
-  print(part1(*parse(sys.stdin)))
+  print(part2(*parse(sys.stdin)))
